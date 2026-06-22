1ba14004dd642dd45414816cc97a9ec55d3cecfb
Franconicovicente <franconicolassvicente@gmail.com>
2026-06-22 12:25:21 -0300

    no funciona back, siguen los test

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    MongooseModule.forRoot('mongodb://gestion_user:Franconi08%21@cluster0-shard-00-00.lyfoszr.mongodb.net:27017,cluster0-shard-00-01.lyfoszr.mongodb.net:27017,cluster0-shard-00-02.lyfoszr.mongodb.net:27017/tp_red_social?ssl=true&replicaSet=atlas-lyfoszr-shard-0&authSource=admin&retryWrites=true&w=majority'),
-    AuthModule,
-  ],
-  controllers: [],
-  providers: [],
+  ConfigModule.forRoot({
+    envFilePath: '.env', // 👈 Le especificamos el archivo exacto
+    isGlobal: true,
+  }),
+  MongooseModule.forRootAsync({
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService) => {
+    const uri = configService.get<string>('MONGO_URI');
+    console.log('🔌 Intentando conectar a:', uri);
+    return {
+      uri,
+      family: 4, 
+      connectTimeoutMS: 10000, 
+    };
+  },
+  inject: [ConfigService],
+}),
+  AuthModule,
+],
 })
 export class AppModule {}
